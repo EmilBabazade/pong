84c780221d63bfa0a8c6e0ab1c1c65f84f526e5c
Emil <emilbabazade29@gmail.com>
2025-01-17 21:43:59 +0200

    scoring system

--- scenes/main.gd
+++ scenes/main.gd
@@ -1,6 +1,8 @@
 extends Node2D
 
 @onready var ball_scene: PackedScene = preload("res://scenes/objects/ball.tscn")
+var player1_score = 0
+var player2_score = 0
 
 func _ready() -> void:
 	spawn_ball()
@@ -14,13 +16,17 @@ func goal():
 	var ball: Node2D = get_tree().get_first_node_in_group('ball')
 	if ball:
 		ball.queue_free()
-		await get_tree().create_timer(1).timeout
+		await get_tree().create_timer(0.5).timeout
 		spawn_ball()
 
 
 func _on_player_1_goal_body_entered(body: Node2D) -> void:
+	player2_score += 1
+	$UI.set_player2_score(player2_score)
 	goal()
 
 
 func _on_player_2_goal_body_entered(body: Node2D) -> void:
+	player1_score += 1
+	$UI.set_player1_score(player1_score)
 	goal()
---
+++ scenes/user_interface/ui.gd
@@ -0,0 +1,7 @@
+extends CanvasLayer
+
+func set_player1_score(score: int):
+	$Player1Score/Label.text = str(score)
+
+func set_player2_score(score: int):
+	$Player2Score/Label.text = str(score)
